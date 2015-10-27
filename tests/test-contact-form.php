06558307f0926e81c94695745570c9c57876d7d0
Sander de Jong <sander@recras.nl>
2015-10-27 16:12:36 +0100

    Add more contact form tests

--- tests/test-contact-form.php
+++ tests/test-contact-form.php
@@ -23,13 +23,77 @@ class ContactFormTest extends \WP_UnitTestCase
         $this->assertEquals('Error: ID is not a number' . "\n", $content, 'Non-numeric ID should fail');
 	}
 
+    function testRegularContactForm()
+    {
+        $post = $this->factory->post->create_and_get([
+            'post_content' => '[recras-contact id=3]'
+        ]);
+        $content = apply_filters('the_content', $post->post_content);
+        $this->assertNotFalse(strpos($content, '<h2'), 'Regular contact form should include a title');
+        $this->assertNotFalse(strpos($content, '<dl'), 'Regular contact form should be a definition list');
+        $this->assertNotFalse(strpos($content, '<dt'), 'Regular contact form should include definition terms');
+        $this->assertNotFalse(strpos($content, '<dd'), 'Regular contact form should include definitions');
+        $this->assertNotFalse(strpos($content, '<label'), 'Regular contact form should show labels');
+        $this->assertNotFalse(strpos($content, 'placeholder='), 'Regular contact form should show placeholders');
+    }
+
     function testContactformNoTitle()
     {
         $post = $this->factory->post->create_and_get([
             'post_content' => '[recras-contact id=3 showtitle=false]'
         ]);
         $content = apply_filters('the_content', $post->post_content);
-        $this->assertFalse(strpos($content, '<h3'), 'Setting showtitle to false should not generate a title');
+        $this->assertFalse(strpos($content, '<h2'), 'Setting showtitle to false should not generate a title');
+    }
+
+    function testContactformNoLabels()
+    {
+        $post = $this->factory->post->create_and_get([
+            'post_content' => '[recras-contact id=3 showlabels="no"]'
+        ]);
+        $content = apply_filters('the_content', $post->post_content);
+        $this->assertFalse(strpos($content, '<label'), 'Setting showlabels to false should not generate labels');
+    }
+
+    function testContactformNoPlaceholders()
+    {
+        $post = $this->factory->post->create_and_get([
+            'post_content' => '[recras-contact id=3 showplaceholders=0]'
+        ]);
+        $content = apply_filters('the_content', $post->post_content);
+        $this->assertFalse(strpos($content, 'placeholder='), 'Setting showplaceholders to false should not generate placeholders');
+    }
+
+    function testContactformAsOrderedList()
+    {
+        $post = $this->factory->post->create_and_get([
+            'post_content' => '[recras-contact id=3 element=ol]'
+        ]);
+        $content = apply_filters('the_content', $post->post_content);
+        $this->assertFalse(strpos($content, '<dl'), 'Contact form with element set to ol should not be a definition list');
+        $this->assertNotFalse(strpos($content, '<ol'), 'Contact form with element set to ol should be an ordered list');
+        $this->assertNotFalse(strpos($content, '<li'), 'Contact form with element set to ol should have list items');
+    }
+
+    function testContactformAsTable()
+    {
+        $post = $this->factory->post->create_and_get([
+            'post_content' => '[recras-contact id=3 element=table]'
+        ]);
+        $content = apply_filters('the_content', $post->post_content);
+        $this->assertFalse(strpos($content, '<dl'), 'Contact form with element set to ol should not be a definition list');
+        $this->assertNotFalse(strpos($content, '<table'), 'Contact form with element set to table should be a table');
+        $this->assertNotFalse(strpos($content, '<tr'), 'Contact form with element set to table should have at least one row');
+        $this->assertNotFalse(strpos($content, '<td'), 'Contact form with element set to table should have at least one cell');
+    }
+
+    function testSubmitDifferentText()
+    {
+        $post = $this->factory->post->create_and_get([
+            'post_content' => '[recras-contact id=3 submittext="Ni!"]'
+        ]);
+        $content = apply_filters('the_content', $post->post_content);
+        $this->assertNotFalse(strpos($content, '<input type="submit" value="Ni!">'), 'Changing submit button text should work');
     }
 
     function testGetForms()
