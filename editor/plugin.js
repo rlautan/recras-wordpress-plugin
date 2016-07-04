1bff49d65c43e7b8bdbcb55d6ea16cacd13e2497
Sander de Jong <sander@recras.nl>
2016-07-04 14:22:01 +0200

    Different way of initialising the TinyMCE plugin

--- editor/plugin.js
+++ editor/plugin.js
@@ -1,54 +1,35 @@
-(function() {
-    tinymce.create('tinymce.plugins.recras', {
-        /**
-         * Initializes the plugin, this will be executed after the plugin has been created.
-         * This call is done before the editor instance has finished it's initialization so use the onInit event
-         * of the editor instance to intercept that event.
-         */
-        init : function(ed, url) {
-            ed.addButton('recras-arrangement', {
-                title : 'Arrangement',
-                image : url + '/arrangement.svg',
-                onclick : function(){
-                    tb_show('Arrangement', 'admin.php?page=form-arrangement');
-                }
-            });
-
-            ed.addButton('recras-booking', {
-                title : 'Online booking',
-                image : url + '/calendar.svg',
-                onclick : function(){
-                    tb_show('Online booking', 'admin.php?page=form-booking');
-                }
-            });
-
-            ed.addButton('recras-contact', {
-                title : 'Contact Form',
-                image : url + '/contact.svg',
-                onclick : function(){
-                    tb_show('Contact', 'admin.php?page=form-contact');
-                }
-            });
-
-            ed.addButton('recras-product', {
-                title : 'Product',
-                image : url + '/product.svg',
-                onclick : function(){
-                    tb_show('Contact', 'admin.php?page=form-product');
-                }
-            });
-        },
-
-        getInfo : function() {
-            return {
-                longname : 'Recras arrangement shortcode',
-                author : 'Recras',
-                authorurl : 'https://www.recras.nl/',
-                infourl : 'https://www.recras.nl/',
-                version : "1.0.0"
-            };
+var recrasPlugin = function(editor, url) {
+    editor.addButton('recras-arrangement', {
+        title : 'Arrangement',
+        image : url + '/arrangement.svg',
+        onclick : function(){
+            tb_show('Arrangement', 'admin.php?page=form-arrangement');
         }
     });
 
-    tinymce.PluginManager.add('recras', tinymce.plugins.recras);
-})();
+    editor.addButton('recras-booking', {
+        title : 'Online booking',
+        image : url + '/calendar.svg',
+        onclick : function(){
+            tb_show('Online booking', 'admin.php?page=form-booking');
+        }
+    });
+
+    editor.addButton('recras-contact', {
+        title : 'Contact Form',
+        image : url + '/contact.svg',
+        onclick : function(){
+            tb_show('Contact', 'admin.php?page=form-contact');
+        }
+    });
+
+    editor.addButton('recras-product', {
+        title : 'Product',
+        image : url + '/product.svg',
+        onclick : function(){
+            tb_show('Contact', 'admin.php?page=form-product');
+        }
+    });
+};
+
+tinymce.PluginManager.add('recras', recrasPlugin);
