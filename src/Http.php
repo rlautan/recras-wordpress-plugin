38d21e7c34255ee65d702105bcb186253c40af06
Sander de Jong <sander@recras.nl>
2017-05-22 16:47:13 +0200

    Use curl for retrieving Recras data

--- src/Http.php
+++ src/Http.php
@@ -3,6 +3,24 @@ namespace Recras;
 
 class Http
 {
+    private static function jsonErrorMessage($errorCode) {
+        switch ($errorCode) {
+            case \JSON_ERROR_DEPTH:
+                return __('The maximum stack depth has been exceeded', Plugin::TEXT_DOMAIN);
+            case \JSON_ERROR_STATE_MISMATCH:
+                return __('Invalid or malformed JSON', Plugin::TEXT_DOMAIN);
+            case \JSON_ERROR_CTRL_CHAR:
+                return __('Control character error, possibly incorrectly encoded', Plugin::TEXT_DOMAIN);
+            case \JSON_ERROR_SYNTAX:
+                return __('Syntax error', Plugin::TEXT_DOMAIN);
+            case \JSON_ERROR_UTF8:
+                return __('Malformed UTF-8 characters, possibly incorrectly encoded', Plugin::TEXT_DOMAIN);
+            default:
+                return __('Unknown JSON error', Plugin::TEXT_DOMAIN);
+        }
+    }
+
+
     /**
      * @param string $subdomain
      * @param string $uri
@@ -15,14 +33,29 @@ class Http
      */
     public static function get($subdomain, $uri, $api = 'api2.php')
     {
-        $json = @file_get_contents('https://' . $subdomain . '.recras.nl/' . $api . '/' . $uri);
+        $ch = curl_init('https://' . $subdomain . '.recras.nl/' . $api . '/' . $uri);
+        curl_setopt($ch, \CURLOPT_RETURNTRANSFER, 1);
+        $json = curl_exec($ch);
+
         if ($json === false) {
-            throw new Exception\UrlException(__('Error: could not retrieve data from Recras', Plugin::TEXT_DOMAIN));
+            $errorMsg = curl_error($ch);
+            /*$statusCode = curl_getinfo($ch, \CURLINFO_HTTP_CODE);
+            if ($statusCode != 200) {
+                $errorMsg .= ' (HTTP ' . $statusCode . ')';
+            }*/
+            throw new Exception\UrlException(sprintf(__('Error: could not retrieve data from Recras. The error message received was: %s', Plugin::TEXT_DOMAIN), $errorMsg));
         }
         $json = json_decode($json);
         if (is_null($json)) {
-            throw new Exception\JsonParseException(__('Error: could not parse data from Recras', Plugin::TEXT_DOMAIN));
+            if (function_exists('json_last_error_msg')) {
+                $errorMsg = json_last_error_msg();
+            } else {
+                $errorMsg = self::jsonErrorMessage(json_last_error());
+            }
+            throw new Exception\JsonParseException(sprintf(__('Error: could not parse data from Recras. The error message was: %s', Plugin::TEXT_DOMAIN), $errorMsg));
         }
+
+        curl_close($ch);
         return $json;
     }
 }
