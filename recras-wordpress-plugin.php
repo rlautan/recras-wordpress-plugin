c0e5521e88ed88d336fc68213e7058cd696d7be4
Sander de Jong <sander@recras.nl>
2018-12-03 16:55:10 +0100

    PHP 7.0 reached EOL

--- recras-wordpress-plugin.php
+++ recras-wordpress-plugin.php
@@ -25,7 +25,7 @@ try {
         function phpVersionTooOld() {
             echo '<div class="error"><p>';
             echo 'The Recras WordPress plugin requires PHP 5.4 or higher but your server is running PHP version ' . phpversion() . '. The plugin will not be activated.';
-            echo 'Any PHP version before than 5.6.0 has reached "End of Life" and no longer receives bugfixes or security updates.';
+            echo 'All PHP versions before 5.6.0, as well as PHP 7.0 have reached "End of Life" and no longer receive bugfixes or security updates.';
             echo 'The official information on how to update and why can be found on <a href="https://secure.php.net/supported-versions.php" target="_blank"><strong>the PHP website</strong></a>.';
             echo '</p></div>';
         }
