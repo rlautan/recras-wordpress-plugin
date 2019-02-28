293ebf0758512838662174ed9b1785813645d9f1
Sander de Jong <sander@recras.nl>
2019-02-28 16:41:35 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,51 +1,77 @@
 #! /bin/bash
-# See https://github.com/GaryJones/wordpress-plugin-git-flow-svn-deploy for instructions and credits.
+# See https://github.com/GaryJones/wordpress-plugin-svn-deploy for instructions and credits.
+
 
 # Set up some default values. Feel free to change these in your own script
 PLUGINSLUG="recras"
-CURRENTDIR=`pwd`
+CURRENTDIR=$(pwd)
 PLUGINDIR="$CURRENTDIR"
-GITPATH="$PLUGINDIR/" # this file should be in the base of your git repository
 MAINFILE="recras-wordpress-plugin.php"
 SVNPATH="/tmp/$PLUGINSLUG"
 SVNURL="https://plugins.svn.wordpress.org/$PLUGINSLUG"
 SVNUSER="ZanderZ"
 
-echo "..............................................."
 echo
-echo "WordPress Plugin Git-Flow SVN Deploy v2.1.0"
-echo
-echo "Preparing to deploy WordPress plugin"
-echo
-echo "..............................................."
+echo "WordPress Plugin SVN Deploy v3.0.0"
 echo
 
-# Check version in readme.txt is the same as plugin file after translating both to unix line breaks to work around grep's failure to identify mac line breaks
-PLUGINVERSION=`grep -i "Version:" $GITPATH/$MAINFILE | awk -F' ' '{print $NF}' | tr -d '\r'`
+# Check main plugin file exists.
+if [ ! -f "$PLUGINDIR/$MAINFILE" ]; then
+  echo "Plugin file $PLUGINDIR/$MAINFILE not found. Aborting."
+  exit 1;
+fi
+
+echo "Checking version in main plugin file matches version in readme.txt file..."
+echo
+
+# Check version in readme.txt is the same as plugin file after translating both to Unix line breaks to work around grep's failure to identify Mac line breaks
+PLUGINVERSION=$(grep -i "Version:" $PLUGINDIR/$MAINFILE | awk -F' ' '{print $NF}' | tr -d '\r')
 echo "$MAINFILE version: $PLUGINVERSION"
-READMEVERSION=`grep -i "Stable tag:" $GITPATH/readme.txt | awk -F' ' '{print $NF}' | tr -d '\r'`
+READMEVERSION=$(grep -i "Stable tag:" $PLUGINDIR/readme.txt | awk -F' ' '{print $NF}' | tr -d '\r')
 echo "readme.txt version: $READMEVERSION"
 
 if [ "$READMEVERSION" = "trunk" ]; then
-	echo "Version in readme.txt & $MAINFILE don't match, but Stable tag is trunk. Let's proceed..."
+	echo "Version in readme.txt & $MAINFILE don't match, but Stable tag is trunk. Let's continue..."
 elif [ "$PLUGINVERSION" != "$READMEVERSION" ]; then
 	echo "Version in readme.txt & $MAINFILE don't match. Exiting...."
 	exit 1;
 elif [ "$PLUGINVERSION" = "$READMEVERSION" ]; then
-	echo "Versions match in readme.txt and $MAINFILE. Let's proceed..."
+	echo "Versions match in readme.txt and $MAINFILE. Let's continue..."
 fi
 
-echo "Changing to $GITPATH"
-cd $GITPATH
+# Let's begin...
+echo ".........................................."
+echo
+echo "Preparing to deploy WordPress plugin"
+echo
+echo ".........................................."
+echo
 
 echo
-echo "Creating local copy of SVN repo trunk ..."
+
+echo "Changing to $PLUGINDIR"
+cd $PLUGINDIR
+
+# Check for git tag (may need to allow for leading "v"?)
+# if git show-ref --tags --quiet --verify -- "refs/tags/v$PLUGINVERSION"
+if git show-ref --tags --quiet --verify -- "refs/tags/v$PLUGINVERSION"
+	then
+		echo "Git tag v$PLUGINVERSION does exist. Let's continue..."
+	else
+		echo "v$PLUGINVERSION does not exist as a git tag. Aborting.";
+		exit 1;
+fi
+
+echo
+
+echo "Creating local copy of SVN repo trunk..."
 svn checkout $SVNURL $SVNPATH --depth immediates
 svn update --quiet $SVNPATH/trunk --set-depth infinity
 
-echo "Ignoring certain files"
+echo "Ignoring GitHub specific files"
 svn propset svn:ignore "README.md
 Thumbs.db
+.github/*
 .git
 .gitattributes
 .gitignore" "$SVNPATH/trunk/"
@@ -72,15 +98,19 @@ if [ -f ".gitmodules" ]
 			done
 fi
 
+echo
+
 # Support for the /assets folder on the .org repo.
-echo "Moving assets"
+echo "Moving assets."
 # Make the directory if it doesn't already exist
 mkdir -p $SVNPATH/assets/
 mv $SVNPATH/trunk/assets/* $SVNPATH/assets/
 svn add --force $SVNPATH/assets/
 svn delete --force $SVNPATH/trunk/assets
 
-echo "Changing directory to SVN and committing to trunk"
+echo
+
+echo "Changing directory to SVN and committing to trunk."
 cd $SVNPATH/trunk/
 # Delete all files that should not now be added.
 svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2"@"}' | xargs svn del
@@ -88,27 +118,34 @@ svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2"@"}' | xargs s
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2"@"}' | xargs svn add
 svn commit --username=$SVNUSER -m "Preparing for $PLUGINVERSION release"
 
-echo "Updating WordPress plugin repo assets and committing"
+echo
+
+echo "Updating WordPress plugin repo assets and committing."
 cd $SVNPATH/assets/
 # Delete all new files that are not set to be ignored
 svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2"@"}' | xargs svn del
 # Add all new files that are not set to be ignored
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2"@"}' | xargs svn add
 svn update --quiet --accept working $SVNPATH/assets/*
+svn resolve --accept working $SVNPATH/assets/*
 svn commit --username=$SVNUSER -m "Updating assets"
 
-echo "Creating new SVN tag and committing it"
+echo
+
+echo "Creating new SVN tag and committing it."
 cd $SVNPATH
-svn update --quiet $SVNPATH/tags/$PLUGINVERSION
 svn copy --quiet trunk/ tags/$PLUGINVERSION/
 # Remove assets and trunk directories from tag directory
 svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/assets
 svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/trunk
 svn update --quiet --accept working $SVNPATH/tags/$PLUGINVERSION
+#svn resolve --accept working $SVNPATH/tags/$PLUGINVERSION/*
 cd $SVNPATH/tags/$PLUGINVERSION
 svn commit --username=$SVNUSER -m "Tagging version $PLUGINVERSION"
 
-echo "Removing temporary directory $SVNPATH"
+echo
+
+echo "Removing temporary directory $SVNPATH."
 cd $SVNPATH
 cd ..
 rm -fr $SVNPATH/
