918b4d102401d662abe65d935f2ebff932457f71
Sander de Jong <sander@recras.nl>
2019-05-03 15:53:56 +0200

    Styling fix for "Recras Blue" theme

--- src/Settings.php
+++ src/Settings.php
@@ -165,7 +165,7 @@ class Settings
             ],
             'recrasblue' => [
                 'name' => __('Recras Blue', Plugin::TEXT_DOMAIN),
-                'version' => '2.0.5',
+                'version' => '2.4.1',
             ],
         ];
     }
