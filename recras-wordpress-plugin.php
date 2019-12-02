a96bf78674824ede97216013b405ced5680493ca
Sander de Jong <sander@recras.nl>
2019-12-02 12:22:21 +0100

    PHP 7.1 is EOL

--- recras-wordpress-plugin.php
+++ recras-wordpress-plugin.php
@@ -21,19 +21,19 @@ if (!function_exists('add_action')) {
     die('You cannot run this file directly.');
 }
 
-try {
-    require_once('vendor/autoload.php');
-    $recrasPlugin = new \Recras\Plugin;
-} catch (Exception $e) {
-    if (version_compare(phpversion(), '5.6', '<')) {
-        function phpVersionTooOld() {
-            echo '<div class="error"><p>';
-            echo 'The Recras WordPress plugin requires PHP 5.6 or higher but your server is running PHP version ' . phpversion() . '. The plugin will not be activated.';
-            echo 'All PHP versions before 7.1.0 have reached "End of Life" and no longer receive bugfixes or security updates.';
-            echo 'The official information on how to update and why can be found on <a href="https://www.php.net/supported-versions.php" target="_blank"><strong>the PHP website</strong></a>.';
-            echo '</p></div>';
-        }
-        add_action('admin_notices', 'phpVersionTooOld');
-        return;
+if (version_compare(phpversion(), '5.6', '<')) {
+    function phpVersionTooOld() {
+        echo '<div class="error">';
+        echo '<h2>Recras WordPress plugin</h2>';
+        echo '<p>';
+        echo 'The Recras WordPress plugin requires PHP 5.6 or higher but your server is running PHP version ' . phpversion() . '. The plugin will not be activated.<br>';
+        echo 'All PHP versions before 7.2.0 have reached "End of Life" and no longer receive bugfixes or security updates. ';
+        echo 'The official information on how to update and why can be found on <a href="https://www.php.net/supported-versions.php" target="_blank"><strong>the PHP website</strong></a>.';
+        echo '</p></div>';
     }
+    add_action('admin_notices', 'phpVersionTooOld');
+    return;
 }
+
+require_once('vendor/autoload.php');
+$recrasPlugin = new \Recras\Plugin;
